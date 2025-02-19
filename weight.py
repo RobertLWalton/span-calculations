f84ea7f8d971f122c25525bb4e1eca63f47ff93b
Robert L Walton <walton@acm.org>
2025-02-19 02:25:10 -0500

    update

--- weight.py
+++ weight.py
@@ -4,7 +4,7 @@
 #
 # File:         weight.py
 # Authors:      Bob Walton (walton@acm.org)
-# Date:         Tue Feb 18 10:04:07 AM EST 2025
+# Date:         Wed Feb 19 12:56:12 AM EST 2025
 #
 # The authors have placed this program in the public
 # domain; they make no warranty and accept no liability
@@ -43,16 +43,18 @@ widths = { "2x4":  1.5, "4x4":  3.5,
            "2x6":  1.5, "4x6":  3.5,
            "2x8":  1.5, "4x8":  3.5,
            "2x10": 1.5, "4x10": 3.5,
-           "2x12": 1.5, "4x10": 3.5 }
+           "2x12": 1.5, "4x12": 3.5 }
 heights = { "2x4":  3.5,   "4x4":  3.5,
             "2x6":  5.5,   "4x6":  5.5,
             "2x8":  7.25,  "4x8":  7.25,
             "2x10": 9.25,  "4x10": 9.25,
-            "2x12": 11.25, "4x10": 11.25 }
-dimensions = [ "4x4", "2x6", "4x6", "2x8",
-               "2x10", "2x12" ]
-spans = [ [ 4, 6, 8, 10, 12, 14 ],     # page 2
-          [ 16, 18, 20, 22, 24, 26 ] ] # page 3
+            "2x12": 11.25, "4x12": 11.25 }
+dimensions = [
+    [ "2x4", "2x6", "2x8", "2x10", "2x12" ], # page 1
+    [ "4x4", "4x6", "4x8", "4x10", "4x12" ]  # page 2
+]
+spans = [ [ 4, 6, 8, 10, 12, 14 ],     # column 2
+          [ 16, 18, 20, 22, 24, 26 ] ] # column 3
 
 
 # Design Reference Values
@@ -67,7 +69,7 @@ Fb = { "2x4": CM_Fb * 1500, "4x4": CM_Fb * 1500,
        "2x6": CM_Fb * 1350, "4x6": CM_Fb * 1350,
        "2x8": CM_Fb * 1250, "4x8": CM_Fb * 1250,
        "2x10": 1050, "4x10": 1050,
-       "2x12": 1000, "4x10": 1000 }
+       "2x12": 1000, "4x12": 1000 }
        # CM_Fb included
 Fv = 175
 E = 1600000
@@ -114,90 +116,35 @@ print ( """
 Allowable
 Weight      Is Proportional To
 ----  --------------------------------------------------
-(1)   stringer width, CD, 1/span-squared
+(1)   stringer width, CD, 1/span^2
 (2)   stringer width, CD, 1/span
-(3)   stringer width, deflection limit/span, 1/span-cubed
+(3)   stringer width, deflection limit/span, 1/span^3
 (4)   stringer width, bearing length, 1/span
 """ )
-separator = "-----------+----------------------------" \
-            "--------------------"
-min_W = {}
-for slist in spans:
-    print ( "\f" )
-    print ( ' SPAN FT   |', end='' )
-    for d in dimensions:
-        print ( "{:>8s}".format ( d ), end='' )
-    print ()
-    print ( separator )
-    for s in slist:
-        for d in dimensions:
-            min_W[d] = math.inf
-        print ( "{:6.0f} (1) |".format ( s ), end='' )
-        for d in dimensions:
-            w = widths[d]
-            h = heights[d]
-            Fb_ = CD * Fb[d]  # psi, CM builtin to Fb[d]
-            Sx = ( w * h ** 2 ) / 6  # in^3
-            M_ = ( Sx * Fb_ ) / 12  # ft lbf
-            # M_ = Mload = W * ( s^2 / 8 )
-            W = M_ * 8 / s**2
-            min_W[d] = min ( min_W[d], W )
-            print ( "{:8.2f}".format ( W ), end='' )
-        print ();
-        print ( "       (2) |", end='' )
-        for d in dimensions:
-            w = widths[d]
-            h = heights[d]
-            Fv_ = CM_Fv * CD * Fv
-            # Vload = W * ( s / 2 )
-            # Fv_ = ( 3 * Vload ) / ( 2 * w * h )
-            W = ( Fv_ * 2 * w * h ) / ( 3 * ( s / 2 ) )
-            min_W[d] = min ( min_W[d], W )
-            print ( "{:8.2f}".format ( W ), end='' )
-        print ();
-        print ( "       (3) |", end='' )
-        for d in dimensions:
-            w = widths[d]
-            h = heights[d]
-            E_ = CM_E * E
-            # 1 / limit = ( 5 * W * s^3 * 12 )
-            #           / ( 384 * E_ * ( w * h^3 ) )
-            W = ( 12 * 384 * E_ * w * h**3 ) \
-              / ( limit * 5 * ( 12 * s )**3 * 12 )
-            min_W[d] = min ( min_W[d], W )
-            print ( "{:8.2f}".format ( W ), end='' )
-        print ();
-        print ( "       (4) |", end='' )
-        for d in dimensions:
-            w = widths[d]
-            Fc_perp_ = CM_Fcperp * Fc_perp
-            # Pload = W * s / 2
-            # area = Pload / Fc_perp_
-            # bearing = area / w
-            #         = W * s / ( 2 * Fc_perp_ * w )
-            W = bearing * 2 * Fc_perp_ * w / s
-            min_W[d] = min ( min_W[d], W )
-            print ( "{:8.2f}".format ( W ), end='' )
-        print ( );
-        print ( "       (5) |", end='' )
-        for d in dimensions:
-            print ( "{:8.2f}".format ( min_W[d] ),
-                    end='' )
-        print (); print ( separator )
 
 print ( """
 \f
 NOTES: (1) LRDF for pedestrian bridges requires
            90 lbf / sqft, deflection limit = span/360
-       (2) For two-stringer boardwalk sections with
-           3 ft treads, this is met by
+       (2) Bearing should be increased to
+               2 inches for 2x10's
+               2.25 inches for 2x12's
+       (3) For two-stringer boardwalk sections with
+           3 ft or 4 ft treads, this is met by
+               2x4's for 4 ft span **
                2x6's for 6 ft span
                2x8's for 8 ft span
                2x10's for 10 ft span
                2x12's for 12 ft span
-       (3) For 4 ft treads this is met by the spans
-           just given if the design bearing length is
-           increased to 2.5 inchs for 2x10's and 2x12's
+               4x4's for 5.8 ft span **
+               4x6's for 8 ft span
+               4x8's for 10 ft span
+               4x10's for 13.5 ft span
+               4x12's for 16 ft span
+           (** but NOT for 4 ft treads )
+       (3) If you add a middle stringer (to 3 total)
+           you can increase the span by a factor of
+           the cube root of 1.5 = 1.14.
        (4) If you double the number of stringers (to 4)
            you can increase the span by a factor of
            the cube root of 2 = 1.26.
@@ -205,3 +152,72 @@ NOTES: (1) LRDF for pedestrian bridges requires
            you can increase the span by a factor of
            the cube root of 3 = 1.44.
 """ )
+separator = "-----------+----------------------------" \
+            "--------------------"
+min_W = {}
+for dlist in dimensions:
+    for slist in spans:
+        print ( "\f" )
+        print ( ' SPAN FT   |', end='' )
+        for d in dlist:
+            print ( "{:>8s}".format ( d ), end='' )
+        print ()
+        print ( separator )
+        for s in slist:
+            for d in dlist:
+                min_W[d] = math.inf
+            print ( "{:6.0f} (1) |".format ( s ),
+                    end='' )
+            for d in dlist:
+                w = widths[d]
+                h = heights[d]
+                Fb_ = CD * Fb[d] 
+                    # psi, CM builtin to Fb[d]
+                Sx = ( w * h ** 2 ) / 6  # in^3
+                M_ = ( Sx * Fb_ ) / 12  # ft lbf
+                # M_ = Mload = W * ( s^2 / 8 )
+                W = M_ * 8 / s**2
+                min_W[d] = min ( min_W[d], W )
+                print ( "{:8.2f}".format ( W ), end='' )
+            print ();
+            print ( "       (2) |", end='' )
+            for d in dlist:
+                w = widths[d]
+                h = heights[d]
+                Fv_ = CM_Fv * CD * Fv
+                # Vload = W * ( s / 2 )
+                # Fv_ = ( 3 * Vload ) / ( 2 * w * h )
+                W = ( Fv_ * 2 * w * h ) \
+                  / ( 3 * ( s / 2 ) )
+                min_W[d] = min ( min_W[d], W )
+                print ( "{:8.2f}".format ( W ), end='' )
+            print ();
+            print ( "       (3) |", end='' )
+            for d in dlist:
+                w = widths[d]
+                h = heights[d]
+                E_ = CM_E * E
+                # 1 / limit = ( 5 * W * s^3 * 12 )
+                #           / ( 384 * E_ * ( w * h^3 ) )
+                W = ( 12 * 384 * E_ * w * h**3 ) \
+                  / ( limit * 5 * ( 12 * s )**3 * 12 )
+                min_W[d] = min ( min_W[d], W )
+                print ( "{:8.2f}".format ( W ), end='' )
+            print ();
+            print ( "       (4) |", end='' )
+            for d in dlist:
+                w = widths[d]
+                Fc_perp_ = CM_Fcperp * Fc_perp
+                # Pload = W * s / 2
+                # area = Pload / Fc_perp_
+                # bearing = area / w
+                #         = W * s / ( 2 * Fc_perp_ * w )
+                W = bearing * 2 * Fc_perp_ * w / s
+                min_W[d] = min ( min_W[d], W )
+                print ( "{:8.2f}".format ( W ), end='' )
+            print ( );
+            print ( "       (5) |", end='' )
+            for d in dlist:
+                print ( "{:8.2f}".format ( min_W[d] ),
+                        end='' )
+            print (); print ( separator )
